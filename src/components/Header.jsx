502a1fe691c1da991e19fde07d1dddbd38ed7399
Prashant <prashantrajpuria@gmail.com>
2026-07-13 13:09:32 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,7 +24,7 @@ export default function Header() {
   }
   return (
     <nav className="bg-gray-100 shadow-md sticky top-0 z-50">
-        <div className="flex h-16 max-w-7xl mx-auto items-center justify-between">
+        <div className="flex h-16 max-w-7xl mx-auto p-4 items-center justify-between">
             <h1 className="font-bold text-2xl">🛍 ShopKart</h1>
             <div className="sm:flex w-96 items-center px-2 py-2 border border-gray-300 focus:outline-none focus-within:ring-2 focus-within:ring-orange-400 rounded-full hidden">
                 <Search className="border-r border-gray-400" />
@@ -35,7 +35,7 @@ export default function Header() {
                 <span className="flex relative gap-2 hover:text-orange-500">
                     <span className="absolute top-0 left-4 bg-red-500 text-xs px-1 py-0.1 text-white rounded-full">{totalQuantity}</span>
                     <ShoppingCart />Cart</span> 
-                <button onClick={handleMenuItem} className="flex bg-orange-400 mr-2 py-2 px-5 rounded-lg hover:bg-orange-500 hover:shadow-lg transition-all duration-300 text-white cursor-pointer"> {menuItem ?<X />: <UserRound />}</button>
+                <button onClick={handleMenuItem} className="flex bg-orange-400  py-2 px-2 rounded-xl hover:bg-orange-500 hover:shadow-lg transition-all duration-300 text-white cursor-pointer"> {menuItem ?<X />: <UserRound />}</button>
             </div>
                     </div>{menuItem &&(
                <div className="absolute top-16 z-50 right-0 w-40 transition-all duration-300 opacity-50 hover:translate-y-1">
