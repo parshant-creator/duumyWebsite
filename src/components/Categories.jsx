781e3d2b224e6129e4a10b40cbd23142aebc84bc
Prashant <prashantrajpuria@gmail.com>
2026-07-15 00:18:11 +0530

    categroies-structure-done

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,26 +1,49 @@
 import { useState } from "react";
-import categories from '../data/categories'
-import products from '../data/products'
+import categories from "../data/categories";
+import products from "../data/products";
 import ProductCard from "./ProductCard";
 export default function Categories() {
   const [activeTab, setActiveTab] = useState("For You");
+  const [loading, setLoading] = useState(false);
+  const handleClick = (category) => {
+    setLoading(true);
 
- 
-  const handleClick =(category)=>{
-    setActiveTab(category)
-    console.log(activeTab)
-  }
+    
+    setTimeout(() => {
+      setActiveTab(category);
+      setLoading(false);
+    }, 300);
+  };
+  const filterProducts =
+    activeTab === "For You"
+      ? products
+      : products.filter((product) => product.category === activeTab);
   return (
-    <section className=" mt-12">
+    <section className=" mt-12 min-h-screen bg-gray-50">
       <div className="max-w-7xl mx-auto">
         <ul className="flex gap-6 overflow-x-auto whitespace-nowrap hider-scrollbar">
-       {categories.map((item,index)=>(
-        <li onClick={()=>handleClick(item)} key={index} className={`cursor-pointer transition-all duration-300 ${activeTab===item?"underline decoration-yellow-500 font-semibold":"text-gary-600"}`}>{item}</li>
-       ))}
-       </ul>
-       {products.map((product)=>(
-        <ProductCard product={product}/>
-       ))}
+          {categories.map((item, index) => (
+            <li
+              onClick={() => handleClick(item)}
+              key={index}
+              className={`cursor-pointer transition-all duration-300 ${activeTab === item ? "underline decoration-yellow-500 font-semibold" : "text-gray-600"}`}
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+        {loading? (
+          <h4 className="flex justify-center items-center">Loading...</h4>
+        ):(
+        <div
+          className={`grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 p-4 items-stretch gap-4 transition-all duration-300 ${loading ? "opacity-0 translate-y-4" : "opacity-100 translate-y-0"}`}
+        >
+          {filterProducts.map((product) => (
+            <ProductCard product={product} />
+          ))}
+        </div>
+        )}
+        
       </div>
     </section>
   );
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,11 +1,54 @@
-
-export default function ProductCard({product}) {
-    console.log(product)
+import { Star, Heart, ShoppingCart } from "lucide-react";
+import { useState } from "react";
+export default function ProductCard({ product }) {
+  const [wishList, setWishList] = useState(false);
+  // const handleWishList = ()=>{
+  //   if(wishList===true){
+  //     setWishList(false)
+  //   }else{
+  //     setWishList(true)
+  //   }
+  // }
+  const handleWishList = () => {
+    setWishList((prev) => !prev);
+  };
   return (
-    <div className="max-w-3xl">
-      <p>Name:{product.name}</p>
-      <span>Price:{product.price}</span>
-      <span>Category:{product.category}</span>
+    <div
+      className="bg-gray-100 p-4 relative rounded-sm flex flex-col h-full hover:shadow-lg
+hover:-translate-y-1
+transition-all
+duration-300"
+    >
+      <button
+        onClick={handleWishList}
+        className="cursor-pointer p-2 rounded-full bg-white shadow-md absolute top-2 right-2"
+      >
+        <Heart
+          className={`transition-all duration-200 hover:scale-110 text-red-500 ${wishList ? "fill-red-500" : "fill-red-50"}`}
+        />
+      </button>
+
+      <div className="h-48 flex justify-center items-center">
+        <img className="h-full w-full object-contain" alt="img" />
+      </div>
+      <div className="flex flex-col gap-2 flex-1 text-gray-600">
+        <h4 className="h-12 font-medium hover:text-black hover:underline">
+          {product.name}
+        </h4>
+        <span className="flex gap-2">
+          <Star className="fill-yellow-400 text-yellow-400" size={18} />
+          {product.rating}
+        </span>
+        <p>{product.category}</p>
+        <span className="font-bold text-black">{product.price}</span>
+      </div>
+      <button
+        onClick={() => console.log(product.name)}
+        className="h-10  flex justify-center items-center gap-2 text-white transition hover:bg-orange-500 cursor-pointer  mt-auto w-full rounded-md bg-orange-400"
+      >
+        <ShoppingCart size={18} />
+        Add To Cart
+      </button>
     </div>
-  )
+  );
 }
