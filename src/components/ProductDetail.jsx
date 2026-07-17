bb79fc750f83f66b882f31521bfe416335b3a83f
Prashant <prashantrajpuria@gmail.com>
2026-07-17 23:52:59 +0530

    related-product

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,9 +1,9 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useState } from "react";
 import products from "../data/products";
 import NoProducts from "./NoProducts";
 import { Star } from "lucide-react";
-
+import ProductCard from '../components/ProductCard'
 export default function ProductDetail() {
   const [quantity, setQuantity] = useState(1);
   const { id } = useParams();
@@ -12,6 +12,10 @@ export default function ProductDetail() {
   if (!product) {
     return <NoProducts />;
   }
+  const relatedProduct = products.filter(
+    (item)=>item.category === product.category &&
+    item.id !==product.id
+  )
   const increaseQuantity = () => {
     setQuantity((prev) => prev + 1);
   };
@@ -23,7 +27,7 @@ export default function ProductDetail() {
   return (
     <div className="min-h-screen">
       <div className="max-w-7xl mx-auto px-8 py-16">
-        <div className="flex w-full flex-col  md:flex-row gap-10 items-start border rounded-md shadow-md border-gray-200 p-4 ">
+        <div className="flex w-full flex-col mb-6  md:flex-row gap-10 items-start border rounded-md shadow-md border-gray-200 p-4 ">
           <div className="w-full md:w-96 h-72 bg-gray-200 rounded-lg overflow-hidden">
             <img
               className="w-full h-full object-contain"
@@ -77,15 +81,24 @@ export default function ProductDetail() {
                 +
               </button>
             </div>
-            <button className="bg-orange-400 rounded-md text-white">
-              Add To Cart
-            </button>
-            <button className="bg-orange-400 rounded-md text-white">
-              Buy Now
-            </button>
+            <button className="bg-orange-500 text-white py-3 rounded-md hover:bg-orange-600 transition">
+  Add To Cart
+</button>
+
+<button className="bg-black text-white py-3 rounded-md hover:bg-gray-800 transition">
+  Buy Now
+</button>
           </div>
         </div>
-      </div>
-    </div>
+        <div className="flex overflow-x-auto whitespace-nowrap gap-4 hider-scrollbar">
+           {relatedProduct.map((product)=>(
+      <Link key={product.id} to={`/product/${product.id}`}>
+              <ProductCard product={product} />
+      </Link>
+     ))} 
+     
+        </div>
+    
+    </div></div>
   );
 }
