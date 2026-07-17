7e2a4389b6789c004b95b9506d9d2875106b2e10
Prashant <prashantrajpuria@gmail.com>
2026-07-17 22:10:40 +0530

    categories-icon

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import categories from "../data/categories";
 import products from "../data/products";
 import ProductCard from "./ProductCard";
 import NoProducts from "./NoProducts";
 import SkeletonCard from "./SkeletonCard";
-import { LayoutGrid } from "lucide-react";
 export default function Categories({ searchTerm }) {
   const [activeTab, setActiveTab] = useState("For You");
   const [loading, setLoading] = useState(false);
+  const [showIcons, setShowIcons] = useState(true);
   const handleClick = (category) => {
     setLoading(true);
     setTimeout(() => {
@@ -22,27 +22,52 @@ export default function Categories({ searchTerm }) {
   const filteredProducts = categoryProducts.filter((product) =>
     product.name.toLowerCase().includes(searchTerm.toLowerCase()),
   );
-
+  const lastScrollY = useRef(0);
+  useEffect(() => {
+    const scroll = () => {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY > lastScrollY.current) {
+        setShowIcons(false);
+      } else {
+        setShowIcons(true);
+      }
+      lastScrollY.current = currentScrollY;
+    };
+    window.addEventListener("scroll", scroll);
+    return () => window.removeEventListener("scroll", scroll);
+  }, []);
   return (
     <section className="mt-2 min-h-screen bg-gray-50">
       <div className="max-w-7xl mx-auto">
-        <div className="flex items-center gap-2 px-2 py-3">
-           <div className="flex items-center gap-2">
-    <LayoutGrid size={22} className="text-orange-500" />
-  </div>
+        <div className="sticky py-4 px-4 top-16 bg-white z-40 shadow-sm border-t border-gray-400">
+          <ul className="flex gap-6 overflow-x-auto whitespace-nowrap hider-scrollbar">
+            {categories.map((item, index) => (
+              <li
+                key={index}
+                onClick={() => handleClick(item.name)}
+                className="flex flex-col items-center gap-2 min-w-[90px] cursor-pointer"
+              >
+                <img
+                  className={`h-8 w-10 transition-all duration-300 overflow-hidden ${
+                    showIcons ? "max-h-10 opacity-100" : "max-h-0 opacity-0"
+                  }`}
+                  src={item.img}
+                  alt={item.name}
+                />
+
+                <span
+                  className={`transition-all duration-300 ${
+                    activeTab === item.name
+                      ? "underline underline-offset-4 decoration-2 decoration-orange-500 font-semibold"
+                      : "text-gray-600"
+                  }`}
+                >
+                  {item.name}
+                </span>
+              </li>
+            ))}
+          </ul>
         </div>
-        <div className="sticky top-16 bg-white z-40 shadow-sm">
-        <ul className=" flex py-4 px-2 gap-6 overflow-x-auto whitespace-nowrap hider-scrollbar border-t border-gray-400 border-b">
-          {categories.map((item, index) => (
-            <li
-              onClick={() => handleClick(item)}
-              key={index}
-              className={`cursor-pointer  transition-all duration-300 ${activeTab === item ? "underline decoration-yellow-500 font-semibold" : "text-gray-600"}`}
-            >
-              {item}
-            </li>
-          ))}
-        </ul></div>
         {loading ? (
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 p-4">
             {Array.from({ length: 6 }).map((_, index) => (
--- src/data/categories.js
+++ src/data/categories.js
@@ -1,16 +1,53 @@
 const categories = [
-  "For You",
-  "Electronics",
-  "Mobile",
-  "Fashion",
-  "Shoes",
-  "Watches",
-  "Beauty",
-  "Furniture",
-  "Men",
-  "Women",
-  "Toys",
-  "Kids",
+  {
+    name: "For You",
+    img: "https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/all.svg",
+  },
+ 
+
+  {
+    name: "Fashion",
+    img: "https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/fashion.svg",
+  },
+  {
+    name: "Mobile",
+    img: "https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/mobiles.svg",
+  }, {
+    name: "Electronics",
+    img: "https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/electronics.svg",
+  },  {
+    name: "Beauty",
+    img: "https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/beauty.svg",
+  },
+  {
+    name: "Home",
+    img: "https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/home-final.svg",
+  },
+  {
+    name: "Appliances",
+    img: "https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/tv.svg",
+  },  {
+    name: "Toys,Baby",
+    img: "https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/toy.svg",
+  },
+
+  {
+    name: "Furniture",
+    img: "https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/furniture.svg",
+  },
+  {
+    name: "Food & Health",
+    img: "https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/food.svg",
+  },
+  {
+    name: "Books & Media",
+    img: "https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/books.svg",
+  },
+
+  {
+    name: "Sports & Fitness",
+    img: "https://static-assets-web.flixcart.com/apex-static/images/svgs/L1Nav/sport.svg",
+  },
 ];
 
 export default categories;
