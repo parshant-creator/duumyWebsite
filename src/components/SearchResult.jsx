a2ca4a43ecc2b3820ccc02209dbea19d9bb9dbd1
Prashant <prashantrajpuria@gmail.com>
2026-07-24 23:12:06 +0530

    sort-app

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -11,7 +11,7 @@ export default function SearchResult() {
   const [selectedCategory, setSelectedCategory] = useState("");
   const [selectedPrice, setSelectedPrice] = useState("");
   const [selectedBrand, setSelectedBrand] = useState("");
-  const [sortBar, setSortBar] = useState(false)
+  const [sortBar, setSortBar] = useState(false);
   const [sortBy, setSortBy] = useState("");
   const [searchParams] = useSearchParams();
   const keyword = searchParams.get("q");
@@ -55,54 +55,81 @@ export default function SearchResult() {
       ? priceProducts
       : priceProducts.filter((product) => product.brand === selectedBrand);
   const showClearButton = selectedCategory || selectedBrand || selectedPrice;
-  const sortedProducts  = [...brandProduct];
-  if(sortBy === "lowToHigh"){
-    sortedProducts.sort((a,b)=>a.price-b.price)
+  const sortedProducts = [...brandProduct];
+  if (sortBy === "lowToHigh") {
+    sortedProducts.sort((a, b) => a.price - b.price);
   }
   if (sortBy === "highToLow") {
-  sortedProducts.sort((a, b) => b.price - a.price);
-}
+    sortedProducts.sort((a, b) => b.price - a.price);
+  }
   const handleClear = () => {
-    (setSelectedCategory(""), setSelectedBrand(""), setSelectedPrice(""), setSortBy(""));
+    (setSelectedCategory(""),
+      setSelectedBrand(""),
+      setSelectedPrice(""),
+      setSortBy(""));
   };
 
-  const handleSortBar = ()=>{
-    setSortBar((sortBar)=>!sortBar)
-  }
+  const handleSortBar = () => {
+    setSortBar((sortBar) => !sortBar);
+  };
 
   return (
     <>
       <Header />
-      <div className="lg:hidden w-full sticky top-28 z-50 border-y bg-white border-gray-200" >
-            <div className="flex justify-between items-center p-4">
-              <button onClick={handleSortBar} className="flex gap-1 items-center" ><ArrowDownWideNarrow /><span>Sort</span></button>
-              <button className="flex gap-1 items-center" ><SlidersHorizontal /><span>Filter</span></button>
-            </div>
+      <div className="lg:hidden w-full sticky top-28 z-50 border-y bg-white border-gray-200">
+        <div className="flex justify-between items-center p-4">
+          <button onClick={handleSortBar} className="flex gap-1 items-center">
+            <ArrowDownWideNarrow />
+            <span>Sort</span>
+          </button>
+          <button className="flex gap-1 items-center">
+            <SlidersHorizontal />
+            <span>Filter</span>
+          </button>
+        </div>
+      </div>
+      {sortBar && (
+        <div
+          onClick={() => setSortBar(false)}
+          className="fixed inset-0 bg-black/40 z-40"
+        >
+          <div className="flex justify-center py-3">
+            <div className="w-12 h-1.5 rounded-full bg-gray-300"></div>
           </div>
-{sortBar && (  <div className="fixed inset-0 bg-black/40 z-40">
-<div className="flex justify-center py-3">
-  <div className="w-12 h-1.5 rounded-full bg-gray-300"></div>
-</div>
-   <div className={ ` bottom-0 fixed w-full bg-white z-50 p-4 border-y transition-transform duration-300 ${sortBar?"translate-y-0":"translate-y-full"}`}>
+          <div
+            onClick={(e) => e.stopPropagation()}
+            className={` bottom-0 fixed w-full bg-white z-50 p-4 border-y transition-transform duration-300 ${sortBar ? "translate-y-0" : "translate-y-full"}`}
+          >
             <h4 className="uppercase mb-2 font-semibold">Sort By</h4>
             <div className="flex flex-col gap-4 mt-2 text-gray-800 font-semibold">
-              <label className="flex gap-2"><span>Price - Low To High</span> 
-              <input type="radio" name="sort" checked={sortBy === "lowToHigh"}
-  onChange={() => {
-    setSortBy("lowToHigh");
-    setSortBar(false);
-  }} />
-            </label>
-            <label className="flex gap-2">
-            <span>Price - High To Low</span>
-            <input type="radio" name="sort" checked={sortBy === "highToLow"}
-onChange={() => {
-  setSortBy("highToLow");
-    setSortBar(false);
-  }} /></label>
-            </div></div>
-          </div>)}
-       
+              <label className="flex gap-2">
+                <span>Price - Low To High</span>
+                <input
+                  type="radio"
+                  name="sort"
+                  checked={sortBy === "lowToHigh"}
+                  onChange={() => {
+                    setSortBy("lowToHigh");
+                    setSortBar(false);
+                  }}
+                />
+              </label>
+              <label className="flex gap-2">
+                <span>Price - High To Low</span>
+                <input
+                  type="radio"
+                  name="sort"
+                  checked={sortBy === "highToLow"}
+                  onChange={() => {
+                    setSortBy("highToLow");
+                    setSortBar(false);
+                  }}
+                />
+              </label>
+            </div>
+          </div>
+        </div>
+      )}
 
       <div className="p-4 min-h-screen">
         <div className="flex justify-between">
@@ -110,14 +137,13 @@ onChange={() => {
             <div className="flex justify-between items-center gap-6 py-4">
               <h4 className="py-2 text-xl font-semibold">Filters</h4>
               {showClearButton && (
-                 <button
-                onClick={handleClear}
-                className="w-full text-sm font-semibold text-blue-600 cursor-pointer"
-              >
-                Clear All
-              </button>
+                <button
+                  onClick={handleClear}
+                  className="w-full text-sm font-semibold text-blue-600 cursor-pointer"
+                >
+                  Clear All
+                </button>
               )}
-             
             </div>
             <hr className="text-gray-400" />
             <h4 className="text-sm font-semibold uppercase py-4">Categories</h4>
@@ -193,12 +219,9 @@ onChange={() => {
               </label>
             </div>
           </div>
-          
 
           <div className="flex-1 px-8">
-            <div
-              className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 p-4"
-            >
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 p-4">
               {sortedProducts.length > 0 ? (
                 sortedProducts.map((product) => (
                   <Link key={product.id} to={`/product/${product.id}`}>
